1ace4323b175c200165047bc6167cf6f301139dd
Tanmay <vermatanmay@hotmail.com>
2026-06-03 22:55:34 +0530

    feat: add user display picture to Topbar component

--- src/app/layout/Topbar/Topbar.tsx
+++ src/app/layout/Topbar/Topbar.tsx
@@ -5,7 +5,10 @@ const Topbar = () => {
     <header className="h-14 flex items-center justify-end p-4 lg:px-6 bg-white topbar">
       <div className="flex items-center gap-3">
         <span className="font-medium hidden md:flex">Welcome, Tanmay</span>
-        <div className="w-8 h-8 rounded-full bg-gray-300" />
+        <img
+          className="w-8 h-8 rounded-full bg-gray-300"
+          src="/src/assets/tanmayDisplayPicture.jpg"
+        />
       </div>
     </header>
   );
